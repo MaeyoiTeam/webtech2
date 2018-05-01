9f1eb080a3c9ad3bfcac2c0950e8887f3abaee9e
jakapatb <max.jakkapat@gmail.com>
2018-05-01 22:31:47 +0700

    แก้ให้สร้างวัน แล้วนศที่ลงจะเพิ่มอัตโนมัติ

--- php/save_coursedate.php
+++ php/save_coursedate.php
@@ -46,17 +46,19 @@
     
          $strSQL = "SELECT coursedate.ID FROM coursedate
         WHERE coursedate.course_ID='".$CourseID."' AND coursedate.sec = '".$Sec."' 
-        AND coursedate.date_date = '".$Date."' AND coursedate.daytime_ID'".$DayTime."'";
+        AND coursedate.date_date = '".$Date."' AND coursedate.daytime_ID = '".$DayTime."'";
         $objQuery = mysqli_query($objCon,$strSQL);
         $coursedate = mysqli_fetch_row($objQuery);
 
         $strSQL = "SELECT studentcourse.ID FROM studentcourse
-        WHERE studentcourse.course_ID='".$_SESSION['course_ID']."' AND studentcourse.sec='".$_SESSION['sec']."')";
-        $objQuery = mysqli_query($objCon,$strSQL);
-        while($row = mysqli_fetch_row($objQuery)){
-            $sql = "INSERT INTO studentcoursedate(studentcourse_ID,coursedate_ID) VALUES ('".$row[0]."','".$coursedate."')"
-            $objQuery = mysqli_query($objCon,$sql);
-
+        WHERE studentcourse.course_ID='".$CourseID."' AND studentcourse.sec='".$Sec."'";
+        
+        if($objQuery2 = mysqli_query($objCon,$strSQL)){
+        while($row = mysqli_fetch_row($objQuery2)){
+            $sql = "INSERT INTO studentcoursedate( `studentcourse_ID`, `coursedate_ID`) VALUES ('".$row[0]."','".$coursedate[0]."')";
+            $objQuery3 = mysqli_query($objCon,$sql);
+        }
+            mysqli_free_result($objQuery2);
 }
         
     echo "<script type='text/javascript'>window.location='../course_teacher.php';</script>";
